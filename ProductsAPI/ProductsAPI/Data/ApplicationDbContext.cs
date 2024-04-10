e8764ec5a5a1d7643ded0211f496cdab5b5d72cb
shady kamel <shady_kamel@yahoo.com>
2024-04-10 23:52:58 +0200

    first commit

---
+++ ProductsAPI/ProductsAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductsAPI.Entities;
+
+namespace ProductsAPI.Data
+{
+    public class ApplicationDbContext
+    {
+        public class AppDbContext : DbContext
+        {
+            public DbSet<Product> Products { get; set; }
+            protected readonly IConfiguration Configuration;
+            public AppDbContext(IConfiguration configuration)
+            {
+                Configuration = configuration;
+            }
+            protected override void OnConfiguring(DbContextOptionsBuilder options)
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+                options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            }
+        }
+    }
+}
---
+++ ProductsAPI/ProductsAPI/Entities/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductsAPI.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
